918ff055635b1ad687d25be1f514aff544b65e7f
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-12-08 20:20:24 -0500

    adding routes for note in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # resources :users
   # resources :notes
  
+  # user authentication routes
   get "/users", to: "users#index"
 
   post "/users", to: "users#create"
@@ -9,4 +10,13 @@ Rails.application.routes.draw do
   
   delete "/users/:id", to: "users#destroy"
   
+
+  # Note routes
+  get "/notes", to: "notes#index"
+  get "/notes/:id", to: "notes#show"
+
+  post "/notes", to: "notes#create"
+
+  
+
 end
