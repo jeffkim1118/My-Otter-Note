528e914e5b113ab0361d6e29fd0baace7de108ac
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-12-15 17:13:25 -0500

    Login form handler added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,10 @@ class SessionsController < ApplicationController
     skip_before_action :authorized, only: :create
 
     def create
-        user = User.find_by(username: params[:username])
-        if user
-            session[:user_id] = user.id
-            render json: user, status: :created
+        @user = User.find_by(username: params[:username])
+        if @user
+            session[:user_id] = @user.id
+            render json: @user, status: :created
         else
             render json: {error: {login: "invalid username or password"}}, status: :unathorized
         end
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,38 @@
+import {useState, useEffect} from 'react';
+
 function Login({setCurrentUser}){
+    const[username, setUsername] = useState('');
+    const[password, setPassword] = useState('');
+    
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const user = {
+            username,
+            password
+        }
+        if(!Object.values(user)){
+            console.log("Enter valid username and password!")
+        }else{
+            fetch(`http://localhost:3000/login`,{
+                method: 'POST',
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(user)
+            }).then((user) => user.json())
+            .then((result) => console.log(result))
+            .catch((error) => console.log(error))
+        }
+    }
+
     return(
         <div className="Login-form">
-            <form>
+            <form onSubmit={handleLogin}>
                 <label>Username:
-                    <input type="text"></input>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}></input>
                 </label>
                 
                 <label>Password:
-                    <input type="text"></input>
+                    <input type="text" value={password} onChange={(e) => setPassword(e.target.value)}></input>
                 </label>
                 
                 <button type="submit">Login</button>
